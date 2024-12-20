9d1f2586f30537d796617e3108c84ba80328b8e4
Luiggi R. Cardoso <68347521+luiggircardoso@users.noreply.github.com>
2024-12-19 23:51:56 -0300

    Change theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,29 +3,89 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import starlightBlog from 'starlight-blog';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'The Blackrabbit Project',
-			plugins: [starlightBlog()],
-			social: {
-				github: 'https://github.com/withastro/starlight',
+  integrations: [
+      starlight({
+          title: 'The Blackrabbit Project',
+          plugins: [starlightBlog()],
+          social: {
+              github: 'https://github.com/withastro/starlight',
+          },
+		  favicon: '/favicon.ico',
+		  logo: {
+			light: './src/assets/branding/no bg/icon-dark.png',
+    		dark: './src/assets/branding/no bg/icon-light.png',
+		  },
+		  head: [
+			// Add ICO favicon fallback for Safari.
+			{
+			  tag: 'link',
+			  attrs: {
+				rel: 'icon',
+				href: '/favicon.ico',
+				sizes: '32x32',
+			  },
 			},
-			sidebar: [
-				{
-					label: 'About',
-					autogenerate: { directory: 'about' }
+			{
+				tag: 'link',
+				attrs: {
+					rel: 'icon',
+					href: '/android-chrome-192x192.png',
+					sizes: '192x192',
 				},
-				{
-					label: 'Contributing',
-					autogenerate: { directory: 'contributing' },
+			},
+			{
+				tag: 'link',
+				attrs: {
+					rel: 'icon',
+					href: '/android-chrome-512x512.png',
+					sizes: '512x512',
 				},
-			],
-			customCss: [
-				'./src/styles/customCss.css',
-				'./src/font/font-face.css'
-			]
-		}),
+			},
+			{
+				tag: 'link',
+				attrs: {
+					rel: 'icon',
+					href: '/apple-touch-icon.png',
+					sizes: '180x180',
+				},
+			},
+			{
+				tag: 'link',
+				attrs: {
+					rel: 'icon',
+					href: '/favicon-16x16.png',
+					sizes: '16x16',
+				},
+			},
+			{
+				tag: 'link',
+				attrs: {
+					rel: 'icon',
+					href: '/favicon-32x32.png',
+					sizes: '32x32',
+				},
+			},
+		  ],
+          sidebar: [
+              {
+                  label: 'About',
+                  autogenerate: { directory: 'about' }
+              },
+              {
+                  label: 'Contributing',
+                  autogenerate: { directory: 'contributing' },
+              },
+          ],
+          customCss: [
+              './src/styles/customCss.css',
+              './src/font/font-face.css'
+          ]
+      }),
 	],
+
+  adapter: netlify(),
 });
